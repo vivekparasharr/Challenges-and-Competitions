33fd20df7b5f5a209a2d8a41f8186cd71d68f64e
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2020-12-17 21:44:54 -0300

    Update 20201208-women-of-2020.py

--- TidyTuesday/20201208-women-of-2020.py
+++ TidyTuesday/20201208-women-of-2020.py
@@ -20,30 +20,21 @@ temp['c'] = [ foo(x) for x in temp['a'] ]
 women = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-12-08/women.csv')
 df = women[['name', 'category', 'country', 'role', 'description']]
 
-'''
-(df >> sift(X.name != 'Unsung hero')
-    >> group_by(X.category)
-    >> summarize(num_of_people = X.name.count())
-).set_index('category').plot(title='# of Women Recognized by Category', y='num_of_people', ylabel='', legend=False, kind='pie')
-'''
-df[df.name != 'Unsung hero'].groupby('category').count()['name']\
-    .plot(title='# of Women Recognized by Category', y='num_of_people', ylabel='', legend=False, kind='pie')
+df[df.name != 'Unsung hero'].groupby('category').count()[['name']]\
+    .plot(title='# of Women Recognized by Category', y='name', ylabel='', legend=False, kind='pie')
 
-(df >> sift(X.name != 'Unsung hero')
-    >> group_by(X.role)
-    >> summarize(num_of_people = X.name.count())
-    >> arrange(X.num_of_people, {ascending=False}) # could not find a way to do this in dplython, so moved to dfply
-)
+df[df.name != 'Unsung hero'].groupby('category').count()[['role']]\
+    .plot(title='# of Women Recognized by Category', y='role', ylabel='', legend=False, kind='bar')
 
 # add continent
 # %conda install -c conda-forge pycountry
 # import pycountry as pc
 
-# pip install geopy
+# %pip install geopy
 # %conda install -c conda-forge geopy
 # from geopy import geocoders
 
-# pip install countryinfo
+# %pip install countryinfo
 from countryinfo import CountryInfo
 # Example
 # CountryInfo('India').info()['region']
@@ -55,15 +46,15 @@ def foo(ctry_nm):
         return 'NA'
 df['continent'] = [ foo(x) for x in df['country']]
 # summarize by continent
-df=df[df.continent != 'NA']
-(df >> group_by(X.continent)
-    >> summarize(num_of_people = X.name.count())
-).set_index('continent').plot(title='# of Women Recognized by Continent', y='num_of_people', ylabel='', legend=False, kind='pie')
+df1 = df[df.name != 'Unsung hero'].groupby(['category','continent']).count()[['name']]\
+    .reset_index()
 
+# Sunburst chart
+import plotly.express as px
+fig=px.sunburst(df1, path=['continent', 'category'], values='name')
+fig.show()
 
-
-
-# Wordcloud
+# Wordcloud (modified to wordcloud by continent)
 # conda install -c conda-forge pillow 
 # conda install -c conda-forge wordcloud
 from os import path
@@ -71,22 +62,27 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-text = str(df.description[0])
-for i in range(1,len(df)):
-    text = text + ' ' + str(df.description[i])
+l = ['Asia','Americas','Europe','Africa']
+for item in l:
+    df1 = df[df.continent==item].reset_index(drop=True)
+    text = str(df1.description[0])
+    for i in range(1,len(df1)):
+        text = text + ' ' + str(df1.description[i])
 
-# Create stopword list: These words wont be included in the word cloud
-stopwords = set(STOPWORDS)
-stopwords.update(['women','S','year','years','name','female'])
+    # Create stopword list: These words wont be included in the word cloud
+    stopwords = set(STOPWORDS)
+    stopwords.update(['women','S','year','years','name','female','Africa','UK'])
 
-# Create and generate a word cloud image:
-# wordcloud = WordCloud().generate(text)
-wordcloud = WordCloud(stopwords=stopwords, max_font_size=50, max_words=100, background_color="white").generate(text)
-# Display the generated image:
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+    # Create and generate a word cloud image:
+    # wordcloud = WordCloud().generate(text)
+    wordcloud = WordCloud(stopwords=stopwords, max_font_size=50, max_words=100, background_color="white").generate(text)
+    # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.title('-'+item+'-', fontsize=24, color='firebrick')
+    plt.show()
 
-# Save the image in the img folder:
-wordcloud.to_file("/Users/vivekparashar/OneDrive/OneDrive-GitHub/Challenges-and-Competitions/TidyTuesday/Data/2020-12-08/first_review.png")
+    # Save the image in the img folder:
+    filepath = os.path.join('/Users/vivekparashar/OneDrive/OneDrive-GitHub/Challenges-and-Competitions/TidyTuesday/Data/2020-12-08/', item+'_word_cloud.png')
+    wordcloud.to_file(filepath)
 
