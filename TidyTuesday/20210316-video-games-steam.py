47804445faa41a8c3f71a5e9a0a20bcf84e18556
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2021-03-23 20:33:48 -0300

    x

--- TidyTuesday/20210316-video-games-steam.py
+++ TidyTuesday/20210316-video-games-steam.py
@@ -4,45 +4,55 @@ import numpy as np
 
 games = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-16/games.csv')
 
+# summary of a database in terms of number of unique elements in each column
+def vp_summ(df):
+    print('#columns:', df.shape[1]) # number of columns
+    print('#rows:', df.shape[0]) # number of rows
+    for r in df.columns:
+        print(r, ':', # column name
+        df[r].unique().shape[0], # number of unique elements in the column
+        '| example:', df[r][0]) # example of the first element in the column
+vp_summ(games)
 
-######  to be updated
+# date time converter
+def vp_dt_convert(conv_type,df,y,m,d):
+    if conv_type=='y-mstr-2dttm': # year and month-str to date time
+        return (pd.to_datetime(df[y].astype(str)  + df[m], format='%Y%B'))
+games['dttm'] = vp_dt_convert('y-mstr-2dttm', games, 'year', 'month','')
 
-df = raw_bechdel.groupby(['year','rating']).nunique()['id'].reset_index()
+# Lets pick up top 6 games
+top_6_games = list(games.groupby('gamename').mean()[['avg','peak']].reset_index().sort_values(by='avg', ascending=False).head(7).gamename[1:7])
+top_6_games = sorted(top_6_games) # sort so that the same color is selected for same game in both charts
 
-map_bechdel = {0:'Unscored movie', 1:'Movie has at least two [named] women in it', 
-    2:'Who talk to each other', 3:'About something besides a man'}
-df['rating_full'] = df['rating'].map(map_bechdel)
+# select only the rows for those 6 games for the line chart
+games[games.gamename.isin(top_6_games)]
 
+# summarize at gamename and year level for selected game for the bar chart
+gsumm = ((games[games.gamename.isin(top_6_games)].groupby(['gamename', 'year']).mean()['avg'])/1000).reset_index()
+gsumm_pivoted = gsumm.pivot(index='year', columns='gamename', values='avg')
 
-import plotly.express as px
-fig = px.area(df[df.year>=1950], x="year", y="id", color='rating_full', 
-        labels={'year':'Year', 'id':'Number of Movies', 'rating_full':'Rating'})
-fig.update_layout(legend=dict(yanchor="top", y=0.8, xanchor="left", x=0.06))
-annotations=[]
-# Title
-annotations.append(dict(xref='paper', yref='paper', x=0.5, y=1.17,
-                        xanchor='center', yanchor='top',
-                        text='Bechdel Test',
-                        font=dict(family='Arial', size=24, color='grey'),
-                        showarrow=False))
-annotations.append(dict(xref='paper', yref='paper', x=0.5, y=1.07,
-                        xanchor='center', yanchor='top',
-                        text='Measuring Hollywood’s gender bias!',
-                        font=dict(family='Arial', size=14, color='grey'),
-                        showarrow=False))
-# Note
-annotations.append(dict(xref='paper', yref='paper', x=0.88, y=0.95,
-                        xanchor='right', yanchor='top',
-                        text='Gender bias seems to have reduced over time.',
-                        font=dict(family='Arial', size=12, color='grey'),
-                        showarrow=False))
-# Footer
-annotations.append(dict(xref='paper', yref='paper', x=0.5, y=-0.20,
-                        xanchor='center', yanchor='top',
-                        text='#TidyTuesday - 2021/03/09 | twitter.com/vivekparasharr | github.com/vivekparasharr',
-                        font=dict(family='Arial', size=12, color='grey'),
-                        showarrow=False))
-fig.update_layout(annotations=annotations)
-fig.show()
+# Plotting
+import matplotlib.pyplot as plt
+import mplcyberpunk
+plt.style.use("cyberpunk")
+cm = 1/2.54  # centimeters in inches
+#px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(30*cm, 20*cm)) #, sharex=True)
+#fig.subplots_adjust(hspace=0.5)
+fig.suptitle('Video Games Data from SteamCharts', fontsize=28, color='white')
+fig.text(0.25, 0.05, 'Note: The charts show Average number of players (in Thousands) at the same time', fontsize = 12, color = "white") 
+fig.text(0.2, 0.01, '#TidyTuesday - 2021/03/16 | twitter.com/vivekparasharr | github.com/vivekparasharr', style = 'italic', fontsize = 14, color = "white") 
+for gname in top_6_games: 
+    ax1.plot(list(games[games.gamename==gname].dttm), list(games[games.gamename==gname].avg/1000), label=gname) # marker='o'
+    #ax2.plot(list(games[games.gamename==gname].dttm), list(games[games.gamename==gname].peak/1000)) # marker='o'
+    #ax2.bar(list(gsumm[gsumm.gamename==gname].year), list(gsumm[gsumm.gamename==gname].avg), label=gname)
+gsumm_pivoted.plot(stacked=True, kind='bar', rot=0, legend=False, ax=ax2)
+ax1.legend(loc='upper center', ncol=3)
+ax2.set_xlabel('Year', size=12)
+mplcyberpunk.add_glow_effects(ax1)
+mplcyberpunk.add_glow_effects(ax2)
+#mplcyberpunk.make_lines_glow(ax1)
+#mplcyberpunk.add_underglow(ax2)
+plt.show()
 
 
---
+++ TidyTuesday/20210323-un-votes.py
@@ -0,0 +1,76 @@
+
+import pandas as pd
+import numpy as np
+
+unvotes = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/unvotes.csv')
+roll_calls = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/roll_calls.csv')
+issues = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/issues.csv')
+
+# votes cast by issue
+issues.groupby('issue').nunique()['rcid']
+
+u2 = unvotes.groupby(['country', 'vote']).count()['rcid'].reset_index()
+u3 = u2.pivot(index='country', columns='vote', values='rcid').reset_index()
+
+# %pip install countryinfo
+from countryinfo import CountryInfo
+# Example
+# CountryInfo('India').info()['region']
+
+def foo(ctry_nm):
+    try:
+        return CountryInfo(ctry_nm).info()['region']
+    except:
+        return 'NA'
+u3['continent'] = [ foo(x) for x in u3['country']]
+# summarize by continent
+u3.groupby(['continent']).sum()[['abstain', 'no', 'yes']].reset_index()
+u3.groupby(['continent']).count()['country'].reset_index()
+
+roll_calls.short.unique()
+
+
+
+
+
+#### to be edited
+
+df = raw_bechdel.groupby(['year','rating']).nunique()['id'].reset_index()
+
+map_bechdel = {0:'Unscored movie', 1:'Movie has at least two [named] women in it', 
+    2:'Who talk to each other', 3:'About something besides a man'}
+df['rating_full'] = df['rating'].map(map_bechdel)
+
+
+import plotly.express as px
+fig = px.area(df[df.year>=1950], x="year", y="id", color='rating_full', 
+        labels={'year':'Year', 'id':'Number of Movies', 'rating_full':'Rating'})
+fig.update_layout(legend=dict(yanchor="top", y=0.8, xanchor="left", x=0.06))
+annotations=[]
+# Title
+annotations.append(dict(xref='paper', yref='paper', x=0.5, y=1.17,
+                        xanchor='center', yanchor='top',
+                        text='Bechdel Test',
+                        font=dict(family='Arial', size=24, color='grey'),
+                        showarrow=False))
+annotations.append(dict(xref='paper', yref='paper', x=0.5, y=1.07,
+                        xanchor='center', yanchor='top',
+                        text='Measuring Hollywood’s gender bias!',
+                        font=dict(family='Arial', size=14, color='grey'),
+                        showarrow=False))
+# Note
+annotations.append(dict(xref='paper', yref='paper', x=0.88, y=0.95,
+                        xanchor='right', yanchor='top',
+                        text='Gender bias seems to have reduced over time.',
+                        font=dict(family='Arial', size=12, color='grey'),
+                        showarrow=False))
+# Footer
+annotations.append(dict(xref='paper', yref='paper', x=0.5, y=-0.20,
+                        xanchor='center', yanchor='top',
+                        text='#TidyTuesday - 2021/03/09 | twitter.com/vivekparasharr | github.com/vivekparasharr',
+                        font=dict(family='Arial', size=12, color='grey'),
+                        showarrow=False))
+fig.update_layout(annotations=annotations)
+fig.show()
+
+
