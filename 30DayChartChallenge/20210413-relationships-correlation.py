d82fdf06a40682e08d67cf2f96e17d18b33cb8f7
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2021-04-13 09:19:44 -0400

    x

--- 30DayChartChallenge/20210413-relationships-correlation.py
+++ 30DayChartChallenge/20210413-relationships-correlation.py
@@ -1,14 +1,16 @@
 
-from altair.vegalite.v4.api import Chart
+# https://www.python-graph-gallery.com/111-custom-correlogram
+# https://seaborn.pydata.org/generated/seaborn.pairplot.html
+
 import numpy as np
 import pandas as pd
-import altair as alt
 
+# Prepare data
 df = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
 df.info()
 
 df.iso_code.unique()
-sel_cols = ['iso_code','total_cases','population',
+sel_cols = ['iso_code','continent','total_cases','population',
     'median_age','life_expectancy','human_development_index']
 exclude_countries = ['OWID_AFR', 'OWID_ASI', 'OWID_EUN', 'OWID_INT', 'OWID_EUR',
 'OWID_KOS', 'OWID_NAM','OWID_CYN', 'OWID_OCE', 'OWID_SAM', 'OWID_WRL']
@@ -17,6 +19,49 @@ df2 = df2[~df2.iso_code.isin(exclude_countries)]
 df2.dropna(inplace=True)
 df2.reset_index(drop=True, inplace=True)
 
+# PLOT
+import seaborn as sns
+import matplotlib.pyplot as plt
+g = sns.pairplot(data=df2[['continent', 'human_development_index', 'life_expectancy', 'median_age']], 
+    kind="scatter", hue="continent", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5), corner=True)
+g.map_lower(sns.kdeplot, levels=4, color=".2")
+g.fig.text(0.35, 0.95,'Correlation between human development index,', fontsize=18) #add text
+g.fig.text(0.35, 0.9,'   life expectancy, and median age by continent', fontsize=18) #add text
+g.fig.text(0.55, 0.85,'Data: https://ourworldindata.org/coronavirus-source-data', fontsize=10) #add text
+g.fig.text(0.55, 0.82,'#30DayChartChallenge - circular - 2021/04/13', fontsize=10) #add text
+g.fig.text(0.55, 0.79,'twitter.com/vivekparasharr | github.com/vivekparasharr', fontsize=10) #add text
+plt.show()
+
+
+
+
+
+
+# UNUSED PLOTS
+
+
+# Basic correlogram
+# kind = (possible kinds are ‘scatter’, ‘kde’, ‘hist’, ‘reg’)
+sns.pairplot(df2[['human_development_index', 'life_expectancy', 'median_age']], 
+    kind="scatter")
+plt.show()
+
+# left
+sns.pairplot(data=df2[['human_development_index', 'life_expectancy', 'median_age']], 
+    kind="scatter", hue="continent", markers=["o", "s", "D"], palette="Set2")
+plt.show()
+
+# You can custom it as a density plot or histogram so see the related sections
+sns.pairplot(df, diag_kind="kde", diag_kws=dict(shade=True, bw_adjust=.05, vertical=False) )
+
+
+
+
+
+# PLOT USING ALTAIR
+
+from altair.vegalite.v4.api import Chart
+import altair as alt
 
 #alt.data_transformers.disable_max_rows()
 chart = alt.Chart(df2).mark_circle().encode(
@@ -30,21 +75,22 @@ chart = alt.Chart(df2).mark_circle().encode(
     row=['median_age', 'life_expectancy', 'human_development_index'],
     column=['human_development_index', 'life_expectancy', 'median_age']
 )
-
-chart.properties(
-    #width=550, height=140, 
-    title={
-      "text": ['Correlation between x, y, and z'], 
-      "fontSize": 18,
-      "font": 'Courier',
-      "anchor": 'middle',
-      "color": 'gray'
-    }
+chart = alt.concat(chart, 
+    title=alt.TitleParams(
+        ['Correlation between x, y, and z'],
+        baseline='top', #'bottom',
+        orient='top', #'bottom', 
+        anchor='middle', #'end',
+        font='Courier',
+        fontWeight='normal',
+        fontSize=18,
+        color='gray'
+    )
 )
 alt.concat(chart, 
     title=alt.TitleParams(
-        ['', '#30DayChartChallenge - correlation - 2021/04/14', 
-        'Dataset: OWID', 
+        ['', '#30DayChartChallenge - correlation - 2021/04/13', 
+        'Dataset: https://ourworldindata.org/coronavirus-source-data', 
         'twitter.com/vivekparasharr | github.com/vivekparasharr | vivekparasharr.medium.com'],
         baseline='bottom',
         orient='bottom',
@@ -54,3 +100,4 @@ alt.concat(chart,
         color='gray'
     )
 )
+
