22fc080874a8b2abbe8f68f7935ddd52739417e7
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2021-09-21 15:47:12 -0400

    Update 20210525-mario-kart-records.py

--- TidyTuesday/20210525-mario-kart-records.py
+++ TidyTuesday/20210525-mario-kart-records.py
@@ -5,23 +5,10 @@ from matplotlib.pyplot import xticks
 import dataprep.eda as eda
 
 records = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/records.csv')
-drivers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/drivers.csv')
+drivers = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/drivers.csv')
 
+records.info()
+records.head(5)
 
-broadband.columns=['ST', 'COUNTY_ID', 'COUNTY_NAME', 'AVAILABILITY', 'USAGE']
+drivers.info()
 
-eda.create_report(records).show_browser()
-
-eda.plot(survey[survey.annual_salary<1000000], 'annual_salary', 'industry').show_browser()
-eda.plot(survey[survey.annual_salary<500000], 'annual_salary', 'industry').show_browser()
-
-eda.plot(
-    survey[survey.annual_salary<500000].replace(to_replace={
-        '5-7 years':'05-07 years', '2 - 4 years':'02-04 years', 
-        '21 - 30 years':'21-30 years', '11 - 20 years':'11-20 years',
-        '1 year or less':'01 year or less', 
-        '8 - 10 years':'08-10 years', '31 - 40 years':'31-40 years',
-        '41 years or more':'41 years or more'
-    }), 'annual_salary', 'years_of_experience_in_field').show_browser()
-
-survey[survey.annual_salary<500000].years_of_experience_in_field.unique()
