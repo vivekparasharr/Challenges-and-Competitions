ec2dceba864255ae41d863c0c1bb7d59bfbc6886
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2021-09-22 18:38:36 -0400

    Update 20210525-mario-kart-records.py

--- TidyTuesday/20210525-mario-kart-records.py
+++ TidyTuesday/20210525-mario-kart-records.py
@@ -7,7 +7,25 @@ import dataprep.eda as eda
 records = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/records.csv')
 drivers = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/drivers.csv')
 
+- separate 'shortcut' records 
+- separate 'type' of record
+- 16 tracks
+- records start from 1996/97
+
+df=records[records.track == 'Luigi Raceway']
+df['year']=pd.DatetimeIndex(pd.to_datetime(df.date)).year
+df.groupby(['track','type','shortcut','year']).mean()['time']
 records.info()
+
+records.system_played.unique()
+
+
+records.groupby('track').count()[['player']].sort_values(by='player', ascending=False).head(10)
+
+df = records.groupby('track').count()[['player']].sort_values(by='player', ascending=False).head(10)
+df.plot(kind='bar')
+
+
 records.head(5)
 
 drivers.info()
